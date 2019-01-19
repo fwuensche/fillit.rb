ab3bb5318e7254f7140f9d1be33689fb8853f0eb
Flavio Wuensche <fwuensch@student.42.us.org>
2019-01-19 15:49:38 +0100

    Add logs with running time and options for verbose

--- main.rb
+++ main.rb
@@ -2,8 +2,11 @@ require_relative "validation"
 require_relative "pieces"
 require_relative "helper"
 require_relative "fillit"
+require_relative "grid"
 
+init_time = Time.now
 filepath = ARGV[0]
+VERBOSE = ["-v", "--verbose"].include?(ARGV[1]) ? "ON" : "OFF"
 filetext = File.read(filepath)
 file_is_valid = validate(filetext)
 
@@ -11,6 +14,7 @@ if file_is_valid
   pieces = read_pieces_from_file(filetext)
   solved_grid = fillit(pieces)
   print_grid(solved_grid)
+  open("logs.out", "a") { |f| f << "#{solved_grid.size},#{Time.now - init_time}\n" }
 else
   print_error(:usage)
 end
